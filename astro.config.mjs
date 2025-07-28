606b5e162e640a5ce7447ef3a06052af1a4b1198
Thien Nguyen <thiennh@proton.me>
2025-07-28 21:03:51 +0700

    ij

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import cloudflare from '@astrojs/cloudflare'; // 👈 Add this
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  output: 'server', // 👈 Required for Cloudflare Functions
+  adapter: cloudflare(), // 👈 Add this
+  integrations: [react()],
 });
